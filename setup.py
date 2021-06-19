d1f2a8b2f337c01a1f54d1054593e53e7a930f3b
mayukhdeb <mayukhmainak2000@gmail.com>
2021-06-19 20:03:14 +0530

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name="eden",
+    version="0.0.0",
+)
