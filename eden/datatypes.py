0dc7fa31fca331445aff5f10128355c5a215b20f
mayukhdeb <mayukhmainak2000@gmail.com>
2021-10-02 20:37:17 +0530

    eden: datatypes: add encode method

--- eden/datatypes.py
+++ eden/datatypes.py
@@ -1,23 +1,36 @@
 from .image_utils import encode
 
 class BaseDataType(object):
-    def __init__(self):
-        self.type = 'eden.datatypes.BaseDataType'
-        self.data = ''
-    
+    def __init__(self, data = None):
+        """
+        A base abstraction over which all other eden's datatypes would be 
+        based on top of. n
 
-    def __call__(self):
+        Args:
+            data: Defaults to None.
+        """
+        self.type = 'eden.datatypes.BaseDataType'
+        self.data = data
+
+    def encode(self):
         return {
             'data': self.data,
             'type': self.type
         }
 
 
-
 class Image(BaseDataType):
     def __init__(self, image = None):
+        """
+        Wrapper to store/send images to and fro from an eden server.
+
+        Args:
+            image (numpy.array or PIL.Image or str, optional): Image to be stored. Defaults to None.
+        """
         super().__init__()
+
         self.type = 'eden.datatypes.Image'
+
         if image is not None:
             self.data = encode(image)
         else:
